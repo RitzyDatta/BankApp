0f08861ef342da354e76e222f159eca88f529db0
Rituparna Datta <ritzy24datta@gmail.com>
2019-08-21 20:40:52 +0530

    make it better

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/BankSearchApplication'));
+app.use(express.static(__dirname + '/dist/BanksSearchApplication'));
 
 app.get('*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+ '/dist/BankSearchApplication/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/BanksSearchApplication/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
